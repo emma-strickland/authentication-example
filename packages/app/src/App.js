2ded0baedb68885bdf0389734fbb0a9594054477
Emma Strickland <emlstrick@gmail.com>
2021-07-16 14:58:31 -0400

    Start building frontend

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -1,59 +1,153 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
-import config from './config';
+
+/*
+1. login server request
+2. get user button & request
+3. pass token from login to getUser and add as header to getUser request
+4. display getUser result 
+*/
 
 function App() {
-  const [response, setResponse] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
 
-  const openSpotifyProfile = () => {
-    window.open(config.SPOTIFY_PROFILE_URL, "_blank");
-  }
-
-  useEffect(() => {
-    fetch(`${config.API_BASE_URL}/recentlyPlayed`)
+  const register = () => {
+    //alert("You are now registered");
+    // make request to server
+    const requestOptionsRegister = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        username: username,
+        password: password
+      })
+    };
+    fetch('http://localhost:4000/register', requestOptionsRegister)
       .then(res => res.json())
       .then(result => {
-        setResponse(result);
+        console.log(result);
       }).catch(error => {
         console.log('error: ', error);
       });
-  }, []);
+  }
+
+  const login = () => {
+    const requestOptionsLogin = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    }
+
+    fetch('http://localhost:4000/login', requestOptionsLogin)
+      .then(res => res.json())
+      .then(result => {
+        setToken(result.token);
+        console.log(result);
+      }).catch(error => {
+        console.log('error: ', error);
+      })
+  }
+
+  const getUser = () => {
+    const requestOptionsGetUser = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer${token}`
+      }
+    }
+
+    fetch('http://localhost:4000/user', requestOptionsGetUser)
+      .then(res => res.json())
+      .then(result => {
+        console.log(result);
+      }).catch(error => {
+        console.log('error: ', error);
+      })
+  }
 
   return (
     <div className="App">
-      {response && (
-        <div className="container">
-          <div className="title">{config.NAME}</div>
-          <div className="subtext">
-            Software Engineer based in Brooklyn, NY
-            <hr />
-            <div className="link">
-              <a href={`mailto:${config.EMAIL_ADDRESS}`} target="_blank" rel="noopener noreferrer">
-                Email
-              </a>
-            </div>
-            <div className="link">
-              <a href={config.LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
-                LinkedIn
-              </a>
-            </div>
-            <div className="link">
-              <a href={config.GITHUB_URL} target="_blank" rel="noopener noreferrer">
-                Github
-              </a>
-            </div>
-            <hr />
-            <div className="current">
-              Currently listening to:
-            </div>
-            <div className="music-text">
-              <img src={response.image} alt="album-art" className="album-art" onClick={openSpotifyProfile} />
-              <div className="music-subtitle">"{response.track}" – {response.artist}</div>
-            </div>
+      <div className="container">
+        <div className="subtext">
+          Authentication Example
+          <hr />
+          <div>
+            <form>
+              <label>
+                First Name:
+                <input
+                  type="text" name="First Name"
+                  onChange={e => setFirstName(e.target.value)}
+                  value={firstName}
+                />
+              </label>
+            </form>
+          </div>
+          <div>
+            <form>
+              <label>
+                Last Name:
+                <input
+                  type="text" name="Last Name"
+                  onChange={e => setLastName(e.target.value)}
+                  value={lastName}
+                />
+              </label>
+            </form>
+          </div>
+          <div>
+            <form>
+              <label>
+                Username:
+                <input
+                  type="text" name="Username"
+                  onChange={e => setUsername(e.target.value)}
+                  value={username}
+                />
+              </label>
+            </form>
+          </div>
+          <div>
+            <form>
+              <label>
+                Password:
+                <input
+                  type="text" name="Password"
+                  onChange={e => setPassword(e.target.value)}
+                  value={password}
+                />
+              </label>
+            </form>
+          </div>
+          <div>
+            <button onClick={register}>
+              Register
+            </button>
+          </div>
+          <div>
+            <button onClick={login}>
+              Login
+            </button>
+          </div>
+          <div>
+            <button onClick={getUser}>
+              Get User
+            </button>
           </div>
         </div>
-      )}
-    </div>
+      </div>
+    </div >
   );
 }
 
